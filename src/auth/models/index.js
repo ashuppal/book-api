d75fb1a25a46be835e4bc4de1242be7ac717f4a3
Ash Uppal <ashuppal@gmail.com>
2023-02-23 10:36:31 -0800

    all tests passing

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -6,7 +6,14 @@ const clothesModel = require('./clothes/model.js');
 const foodModel = require('./food/model.js');
 const Collection = require('./data-collection.js');
 const userModel = require('./users');
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+// const userModel = require('..auth//users/model.js');
+
+//chnaged this durig demo
+// const DATABASE_URL = process.env.NODE_ENV === 'test'? 'sqlite:memory:':process.env.DATABASE_URL;
+const DATABASE_URL = process.env.NODE_ENV === 'test' 
+  ? 'sqlite::memory' 
+  : process.env.DATABASE_URL;
+
 
 const sequelizeDatabase = new Sequelize(DATABASE_URL);
 const food = foodModel(sequelizeDatabase, DataTypes);
